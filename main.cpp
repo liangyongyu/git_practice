ad570b53233327572128fb85440498de1b6d021c
lyy <172459226@qq.com>
2020-02-07 14:40:28 +0800

    pull by others

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ using namespace std;
 int main(int argc, char **argv)
 {
     cout<<"the first practice"<<endl;
-    cout<<"the second practice by lyy"<<endl;
+    cout<<"the  practice by others"<<endl;
     return 0;
 }
