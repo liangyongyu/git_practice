1a13e0ad852150ddc45792752fed9fd080796054
lyy <172459226@qq.com>
2020-02-07 14:30:43 +0800

    first push to github

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by DreamWorks on 2020/2/7.
+//
+#include "iostream"
+using namespace std;
+int main(int argc, char **argv)
+{
+    cout<<"the first practice"<<endl;
+    return 0;
+}
